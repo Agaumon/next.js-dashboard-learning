a83eeb4f79a420a6b116a27490c86fc776aeba9e
Agaumon <159858518+Agaumon@users.noreply.github.com>
2025-12-19 12:42:53 -0500

    Completed Chapter 10

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,82 @@
-export default function Page() {
-    return <p>Invoices Page</p>;
+// export default function Page() { //Original existing code
+//     return <p>Invoices Page</p>;
+// }
+
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/invoices/table';
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { lusitana } from '@/app/ui/fonts';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import { fetchInvoicesPages } from '@/app/lib/data';
+
+// export default async function Page() { //Original code
+export default async function Page(props: { //Replaced original code on line 13 to update the table component to reflect the search query.
+//Page components accept a prop called "searchParams", so you can pass the current URL params to the <Table> component. Lines 16-23
+    searchParams?: Promise<{
+        query?: string;
+        page?: string;
+    }>;
+}) {
+    const searchParams = await props.searchParams;
+    const query = searchParams?.query || '';
+    const currentPage = Number(searchParams?.page) || 1;
+    const totalPages = await fetchInvoicesPages(query); //Passes the query from searchParams. For example, if there are 12 invoices that match the search query, each page will display 6 invoices per page.
+return (
+<div className="w-full">
+    <div className="flex w-full items-center justify-between">
+    <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+    </div>
+    <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+    <Search placeholder="Search invoices..." />
+    <CreateInvoice />
+    </div>
+    {/* Lines 45-47 were uncommented to work with searchParams prop from lines 16-23
+        After all these changes, If you search for a term, you'll update the URL, which will send a new request to the server, data will be fetched on the server, and only the invoices that match your query will be returned.
+        
+        When to use the useSearchParams() hook vs. the searchParams prop?
+
+You might have noticed you used two different ways to extract search params. Whether you use one or the other depends on whether you're working on the client or the server.
+
+- <Search> is a Client Component, so you used the useSearchParams() hook to access the params from the client.
+- <Table> is a Server Component that fetches its own data, so you can pass the searchParams prop from the page to the component.
+
+As a general rule, if you want to read the params from the client, use the useSearchParams() hook as this avoids having to go back to the server.*/}
+
+    <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+    <Table query={query} currentPage={currentPage} />
+    </Suspense>
+    <div className="mt-5 flex w-full justify-center">
+    <Pagination totalPages={totalPages} /> {/*Here we are passing the totalPages prop to the Pagination component.*/}
+    </div>
+</div>
+);
 }
+
+/* 
+1. <Search/> allows users to search for specific invoices.
+2. <Pagination/> allows users to navigate between pages of invoices.
+3. <Table/> displays the invoices.
+
+Your search functionality will span the client and the server. 
+When a user searches for an invoice on the client, the URL params will be updated, data will be fetched on the server, and the table will re-render on the server with the new data.
+
+There are a couple of benefits of implementing search with URL params:
+
+- Bookmarkable and shareable URLs: Since the search parameters are in the URL, users can bookmark the current state of the application, including their search queries and filters, for future reference or sharing.
+- Server-side rendering: URL parameters can be directly consumed on the server to render the initial state, making it easier to handle server rendering.
+- Analytics and tracking: Having search queries and filters directly in the URL makes it easier to track user behavior without requiring additional client-side logic.
+
+These are the Next.js client hooks that you'll use to implement the search functionality:
+
+- useSearchParams- Allows you to access the parameters of the current URL. For example, the search params for this URL /dashboard/invoices?page=1&query=pending would look like this: {page: '1', query: 'pending'}.
+- usePathname - Lets you read the current URL's pathname. For example, for the route /dashboard/invoices, usePathname would return '/dashboard/invoices'.
+- useRouter - Enables navigation between routes within client components programmatically. There are multiple methods you can use.
+
+Here's a quick overview of the implementation steps:
+
+1. Capture the user's input. By using the logic on lines 6-8 and 17-19 in app/ui/search.tsx. This captures the user's input from the search bar on the Invoices page.
+2. Update the URL with the search params.
+3. Keep the URL in sync with the input field.
+4. Update the table to reflect the search query.*/
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,34 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams } from 'next/navigation'; //Added to update URL search params
+import { usePathname, useRouter } from 'next/navigation'; //Added after using 'set' to update URL
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams(); //Added along side line 4 to update search params
+  const pathname = usePathname(); // Our current pathname or URL
+  const { replace } = useRouter(); // Calls the function to replace the current pathname/URL
+
+  // function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term) => {
+      console.log(`Searching... ${term}`);
+    const params = new URLSearchParams(); //Create a new instance using the searchParams variable from line 7
+    params.set('page', '1') //Resets the users current page to 1 as they type in a new search query.
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query')
+    }
+    replace(`${pathname}?${params.toString()}`); //Replaces URL with user's input/search data
+  }, 300);
+    /* URLSearchParams is a Web API that provides utility methods for manipulating the URL query parameters. 
+Instead of creating a complex string literal, you can use it to get the params string like ?page=1&query=a. 
+
+Next, we use 'set' to set the params string based on the user’s input. If the input is empty, you want to delete it on lines 11-15
+
+After writing the code to update the URL based on the search params, we add line 41 to ensure the input field is in sync with the URL and will be populated when sharing, you can pass a defaultValue to input by reading from searchParams. */
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,8 +37,21 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => {
+          handleSearch(e.target.value)
+        }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
   );
 }
+
+/* For every keystroke, our search is querying our database. This is fine for this application since it's small, but when there's a larger application with more data in the database, this can slow things down.
+That's where debouncing comes in. Debouncing is a programming practice that limits the rate at which a function can fire. In our case, you only want to query the database when the user has stopped typing.
+
+How Debouncing Works:
+
+Trigger Event: When an event that should be debounced (like a keystroke in the search box) occurs, a timer starts.
+Wait: If a new event occurs before the timer expires, the timer is reset.
+Execution: If the timer reaches the end of its countdown, the debounced function is executed. */
